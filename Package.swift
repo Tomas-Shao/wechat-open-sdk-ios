5823b7e400e83e71336f06385a18fc149ea80ef5
tomas.shao <tomas.shao@ef.com>
2024-06-01 01:13:41 +0800

    wechat open sdk

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WechatOpenSDK",
+    products: [
+        .library(name: "WechatOpenSDK", targets: ["WechatOpenSDK"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(name: "WechatOpenSDK",
+                      url: "https://github.com/Tomas-Shao/WechatOpenSDK/releases/download/2.0.2/WechatOpenSDK.xcframework.zip",
+                      checksum: "29a849e487e77341b0a14e69c031cb51231cb75a5eee4836436cd606d2091080"),
+    ]
+)
